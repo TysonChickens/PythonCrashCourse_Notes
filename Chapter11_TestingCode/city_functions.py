6219536a16121945f28eb1d35d14096b5d5bfec9
Tyson Nguyen <tyson.nguyen13@gmail.com>
2020-01-01 20:24:12 -0800

    Exercises 1-2 added

--- Chapter11_TestingCode/city_functions.py
+++ Chapter11_TestingCode/city_functions.py
@@ -1,7 +1,24 @@
 # 11-1 City, Country function
 
-def get_city_country(city, country):
-    """Generate a formatted single string of City, Country."""
-    city_country = f"{city}, {country}"
-    return city_country.title()
+# def get_city_country(city, country):
+#     """Generate a formatted single string of City, Country."""
+#     city_country = f"{city}, {country}"
+#     return city_country.title()
     
+
+# 11-2 Modify function so it requires a third parameter, population.
+# This will purposefully fail test case - population is not optional.
+
+# def get_city_country(city, country, population):
+#     """Generate a formatted single string of City, Country."""
+#     city_country = f"{city}, {country} - population {population}"
+#     return city_country.title()
+
+# Population parameter is optional so it will pass.
+def get_city_country(city, country, population=''):
+    """Generate a formatted single string of City, Country."""
+    if population:
+        city_country = f"{city}, {country} - population {population}"
+    else:
+        city_country = f"{city}, {country}"
+    return city_country.title()
--- Chapter11_TestingCode/test_cities.py
+++ Chapter11_TestingCode/test_cities.py
@@ -6,7 +6,11 @@ class CityCountryTestCase(unittest.TestCase):
 
     def test_city_country(self):
         city_country = get_city_country('portland', 'united states')
-        self.assertEqual(city_country, 'Portland, United States')
+        self.assertEqual(city_country, "Portland, United States")
+
+    def test_city_country_population(self):
+        city_country_population = get_city_country('santiago', 'chile', population=5000000)
+        self.assertEqual(city_country_population, "Santiago, Chile - Population 5000000")
 
 if __name__ == '__main__':
     unittest.main()
