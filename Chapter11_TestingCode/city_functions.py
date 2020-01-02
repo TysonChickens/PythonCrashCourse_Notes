1106917c8f60a6f53fca34007e22a6b5819bee84
Tyson Nguyen <tyson.nguyen13@gmail.com>
2020-01-01 19:04:17 -0800

    11-1 setup for city_country test

---
+++ Chapter11_TestingCode/city_functions.py
@@ -0,0 +1,7 @@
+# 11-1 City, Country function
+
+def get_city_country(city, country):
+    """Generate a formatted single string of City, Country."""
+    city_country = f"{city}, {country}"
+    return city_country.title()
+    
---
+++ Chapter11_TestingCode/test_cities.py
@@ -0,0 +1,12 @@
+import unittest
+from city_functions import get_city_country
+
+class CityCountryTestCase(unittest.TestCase):
+    """Tests for city_functions.py."""
+
+    def test_city_country(self):
+        city_country = get_city_country('portland', 'united states')
+        self.assertEqual(city_country, 'Portland, United States')
+
+if __name__ == '__main__':
+    unittest.main()
