2bf7829d12fd2c21f5d4595c53e9d48025708f53
Tyson <tyson.nguyen13@gmail.com>
2020-01-19 21:06:04 -0800

    Rolling dice 50,000 times

--- Projects/DataVisualization/dice_visual.py
+++ Projects/DataVisualization/dice_visual.py
@@ -30,7 +30,7 @@ data = [Bar(x=x_values, y=frequencies)]
 
 x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
-my_layout = Layout(title='Results of rolling a D6 and a D10 dice 1000 times',
+my_layout = Layout(title='Results of rolling a D6 and a D10 dice 50000 times',
     xaxis=x_axis_config, yaxis=y_axis_config)
 offline.plot({'data': data, 'layout': my_layout}, filename='d6_d10.html')
 
